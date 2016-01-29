9a6cf700c32e05c485a768e9f9f5fd2c6fee1334
[sanctumware] <[sysop@sanctumware.net]>
2016-01-29 21:45:56 +0800

    MY MESSAGE HERE

---
+++ jakelib/modules/jake-imagemin.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+var u = require('./node-utility');
+var Imagemin = require('imagemin');
+
+module.exports = function runimagemin(o) {
+  
+  var imagemin = new Imagemin()
+      .src(o.input)
+      .dest(o.output)
+      .use(Imagemin.jpegtran({ progressive: true }));
+  
+  imagemin.run(function (err, files) {
+    if (err) {
+      throw err;
+    }
+    u.print('√ Global Images optimized: ' + o.dest, 'green');
+  });
+  
+}
