f4dca3eec2786cd53bfab8b444a181b66d65da6b
SanctumWare Systems <sysop@sanctumware.net>
2016-06-27 16:58:13 +0800

    added necessary js..

---
+++ js/details.js
@@ -0,0 +1,71 @@
+// Splash
+var splash = ""
++ "+----------------------------------------------------------------------\n"
++ "\n"
++ "             .___   SanctumWare Systems                       \n"              
++ "           __| _/_______  _______ _______   ____ _____        \n"
++ "          / __ |/ __ \  \/ /\__  \\_  __ \_/ __ \\__  \       \n"
++ "         / /_/ \  ___/\   /  / __ \|  | \/\  ___/ / __ \_     \n"
++ "         \____ |\___  >\_/  (____  /__|    \___  >____  /     \n"
++ "              \/    \/           \/            \/     \/ .org \n"
++ "\n"
++ "+----------------------------------------------------------------------\n";
+
+var hasSplash = true;
+
+// Details
+var name = "SanctumWare Systems";
+var loc = "Philippines";
+var lookingfor = "Raking in the money";
+var pdfLink = "http://en.wikipedia.org/wiki/R%C3%A9sum%C3%A9";
+
+// Settings
+var showForks = false; // Shows forks of repositories on Github
+
+// Github and Bitbucket accounts
+var githubUsername = "example";
+var bitbucketUsername = "example";
+var githubURL = "https://github.com/" + githubUsername;
+var bitbucketURL = "https://bitbucket.org/" + bitbucketUsername;
+
+// If left blank e.g. url = "" then it won't appear
+var websiteURL = "http://www.example.com/";
+var blogURL = "";
+var portfolioURL = "";
+var twitterURL = "https://twitter.com/";
+var facebookURL = "https://facebook.com/";
+var skypeAccount = "test";
+var socialMedia = [["Github", githubURL], ["Bitbucket", bitbucketURL], ["Website", websiteURL], 
+	["Blog", blogURL], ["Portfolio", portfolioURL], ["Twitter", twitterURL], ["Facebook", facebookURL],
+	["Skype", skypeAccount]];
+
+// Education list
+var education = [["South Hamptom Institute of Technology", "Master of Computers", "Graduate July, 2014"], ["School of Hard Knocks", "Bachelor of Life", "Graduate November, 2008"]];
+
+// Employment list
+var employment = [["Prestige Worldwide", "Beat Lab Technician", "November 2013 - Present"]];
+
+// Volunteering list
+var volunteering = [["Volunteer", "Voluntation", "November 2013 - Present"]];	
+
+// Awards list
+var awards = [["Mad Coder", "Programmer Institute", "December 2013"]];
+
+// Membership list
+var membership = [["Some Society", "Pizza Box Opener"]];
+
+// Skills list
+var skillsLanguages = [["Proficient", "C/C++ Java Python"],
+	["Experienced", "C# .NET"],
+	["Familiar", "PHP Javascript Bash"]];
+
+var skillsTools = [["Proficient", "Git Wordpress"],
+	["Experienced", "Android Github"],
+	["Familiar", "TCP/IP SVN"]];
+
+var skillsConcepts = [["Proficient", "Kanban Agile"],
+	["Experienced", "Functional Testing"],
+	["Familiar", "Functional Programming"]];
+
+var publicPGPkey = "\n"
+  + "YOUR PGP KEY HERE";
